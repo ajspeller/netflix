3295bd20bcf32f377acdccc0828e6fe66546b7e6
AJ Speller <ajspeller@gmail.com>
2022-01-30 18:54:06 -0500

    Update config files and build add

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.certifyyaself.netflix',
   appName: 'netflix',
   webDir: 'www',
   bundledWebRuntime: false
